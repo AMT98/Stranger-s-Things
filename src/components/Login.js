08642c9650095c1328be772d165a352a84a21ea8
AMT98 <aswinmalla12@gmail.com>
2022-12-12 20:13:57 -0700

    Add Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+
+const Login = ({url}) => {
+    // const handleClick = async () => {
+    //     const response = await fetch(`{url}/posts`)
+    //     const data = response.json()
+    //     console.log(data)
+    //     setPosts(data)
+    // }
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = (e) => {
+        return (
+            e.preventDefault()
+        )
+    }
+
+    return (
+        <>
+        <form
+        onClick={handleSubmit}
+        className='loginInput'>
+        
+                <h1>LOG IN</h1>
+            <label>
+            <input 
+            className='inputField'
+            type="text" 
+            placeholder= "Enter your username" 
+            value={userName} 
+            onChange={(e) => setUserName(e.target.value)}>
+
+            </input>
+            </label>
+
+            <label>
+            <input 
+            className='inputField'
+            type="text" 
+            placeholder= "Enter your password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)}>
+
+            </input>
+            </label>
+            <button>Submit</button>
+        </form>
+        
+        </>
+    )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 
+const App = () => {
+  const url = "https://strangers-things.herokuapp.com/api/2209-FTB-CT-WEB-PT"
+  // const [posts, setPosts] = useState([])
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("app"))
+  return (
+    <Router>
+    <header className='header'>
+      <h1>Stranger's Things </h1>
+      <div>
+        <h2>HOME</h2>
+        <h2>POSTS</h2>
+        <h2>PROFILE</h2> 
+        <Link to="./components/Login.js"><h2>LOG IN</h2></Link>
+      </div>
+    </header>
+        <Switch>
+          <Route path = "./components/Login.js">
+            <Login 
+            url = {url}
+            />
+            {/* <div>
+              hi
+            </div> */}
+          </Route>
+        </Switch>
+      </Router>             
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
