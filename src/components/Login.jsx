7b68036aca3bed2651282602cbd0fa1119682ee4
AMT98 <aswinmalla12@gmail.com>
2022-12-24 15:21:02 -0700

    Delete alert component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import AlertMsg from "./Alert";
+
 
 const Login = ({ url, token }) => {
   let history = useHistory();
